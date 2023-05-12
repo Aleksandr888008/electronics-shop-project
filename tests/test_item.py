e19385253843cdf4ec619db3e308666257c919bf
Александр <learningenglish2804@gmail.com>
2023-05-12 14:41:12 +0400

    add

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
+from src.item import Item
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+def test_calculate_total_price():
+    assert Item.calculate_total_price(10.0, 5) == 50.0
+
+
+def test_apply_discount():
+    assert Item.apply_discount(1, 1.5) == 1.5
